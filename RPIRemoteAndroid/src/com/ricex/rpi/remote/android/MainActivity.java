6ee7a00a08b1e68d7d12a04b904ab7a8af1e1bb6
Mitchell <computers1055@gmail.com>
2013-05-11 21:23:13 -0400

    Created the basis of the android remote

--- RPIRemoteAndroid/src/com/ricex/rpi/remote/android/MainActivity.java
+++ RPIRemoteAndroid/src/com/ricex/rpi/remote/android/MainActivity.java
@@ -1,15 +1,41 @@
 package com.ricex.rpi.remote.android;
 
-import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	/** The action bar of the activity, used for adding the navigational tabs */
+	private ActionBar actionBar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		Tab moviesTab = actionBar.newTab().setText("Movies");
+		Tab controlTab = actionBar.newTab().setText("Controls");
+		Tab playersTab = actionBar.newTab().setText("Players");
+		
+		moviesTab.setTabListener(new TestTabListener(new MoviesListFragment()));
+		controlTab.setTabListener(new TestTabListener(new Fragment()));
+		playersTab.setTabListener(new TestTabListener(new Fragment()));
+		
+		actionBar.addTab(moviesTab);
+		actionBar.addTab(controlTab);
+		actionBar.addTab(playersTab);	
+		
+		
+		
 	}
 
 	@Override
@@ -17,6 +43,34 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private class TestTabListener implements TabListener {
+
+		/** The fragment to switch to when this tab is selected */
+		private Fragment fragment;
+		
+		private TestTabListener(Fragment fragment) {
+			this.fragment = fragment;
+		}
+		
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			//Toast.makeText(getBaseContext(),  "Reselected: " + tab.getText(), Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			//Toast.makeText(getBaseContext(),  "Selected: " + tab.getText(), Toast.LENGTH_SHORT).show();
+			ft.replace(R.id.fragment_container, fragment);
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			//Toast.makeText(getBaseContext(),  "Unselected: " + tab.getText(), Toast.LENGTH_SHORT).show();
+			ft.remove(fragment);
+		}
+		
 	}
 
 }
---
+++ RPIRemoteAndroid/src/com/ricex/rpi/remote/android/MoviesListFragment.java
@@ -0,0 +1,40 @@
+package com.ricex.rpi.remote.android;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+
+public class MoviesListFragment extends Fragment {
+
+	/** The list view that will display the movies in the current directory */
+	private ListView moviesListView;
+	
+	/** The linear layout that will display the navigation buttons */
+	private LinearLayout moviesDirectoryView;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {		
+		View view = inflater.inflate(R.layout.movies_layout, container, false);
+		
+		moviesListView = (ListView) view.findViewById(R.id.movies_list);
+		
+		List<String> data = new ArrayList<String>();
+		for (int i=0;i<15;i++) {
+			data.add("Element " + i);
+		}
+		
+		moviesListView.setAdapter(new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, data));	
+		
+		return view;
+		
+	}
+}
