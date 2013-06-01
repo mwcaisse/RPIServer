cb4eb228d02c23673c0cd58a35249beea0aee8a6
Mitchell <computers1055@gmail.com>
2013-06-01 16:25:53 -0400

    Implemented a basic playlist view around the Playlist model

---
+++ RPIServer/src/com/ricex/rpi/server/player/Playlist.java
@@ -0,0 +1,77 @@
+package com.ricex.rpi.server.player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ricex.rpi.common.video.Video;
+
+/** Play list class which represents a series of movies to play in sequence on a certian device
+ * 
+ * @author Mitchell
+ *
+ */
+
+public class Playlist {
+
+	/** The name of the play list */
+	private final String name;
+	
+	/** The items that the play list will play */
+	private List<Video> items;
+	
+	/** the index of the currently playing item */
+	private int currentIndex;
+	
+	public Playlist(String name) {
+		this.name = name;
+		currentIndex = 0;
+		items = new ArrayList<Video>();
+	}
+	
+	/** Returns the name of this play list */
+	public String getName() {
+		return name;
+	}
+	
+	/** Adds the given item to the end of the play list
+	 * 
+	 * @param item The item to add
+	 */
+	
+	public void addItem(Video item) {
+		items.add(item);
+	}
+	
+	/** Removes the given item 
+	 * 
+	 * @param item The item to remove
+	 */
+	
+	public void removeItem(Video item) {
+		items.remove(item);
+	}
+	
+	/** returns the list of items */
+	public List<Video> getItems() {
+		return items;
+	}
+	
+	/** Returns the next item to be played, or null if at the end 
+	 */	
+	public Video getNextItem() {
+		currentIndex++; //advance to the next item
+		if (items.size() < 1 || items.size() >= currentIndex) {
+			return null;
+		}
+		return items.get(currentIndex);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	
+	@Override
+	public String toString() {
+		return getName();
+	}
+}
--- RPIServer/src/com/ricex/rpi/server/player/PlaylistView.java
+++ RPIServer/src/com/ricex/rpi/server/player/PlaylistView.java
@@ -1,7 +1,17 @@
 package com.ricex.rpi.server.player;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
+
+import com.ricex.rpi.common.video.Movie;
+import com.ricex.rpi.common.video.Video;
 
 /** The view that will display a playlist and allow user to modify / play / create one 
  * 
@@ -13,19 +23,51 @@ public class PlaylistView extends BorderPane {
 
 	
 	/** The listview for displaying the selected playlist items */
-	private ListView<String> listViewPlaylistItems;
+	private ListView<Video> listViewPlaylistItems;
 	
 	/** The list view for displaying the playlists */
-	private ListView<String> listViewPlaylists;
+	private ListView<Playlist> listViewPlaylists;
+	
+	private List<Playlist> playlists;
 	
 	public PlaylistView() {
 	
-		listViewPlaylistItems = new ListView<String>();
+		playlists = new ArrayList<Playlist>();
 		
-		listViewPlaylists = new ListView<String>();
+		for (int i=0;i<10;i++) {
+			Playlist playlist = new Playlist("Playlist " + (i+1));
+			for (int j=0;j<(i+1)*5;j++) {
+				playlist.addItem(new Movie("Playlist " + (i+1) + " Movie " + (j+1)));
+			}
+			playlists.add(playlist);
+		}
+		
+		
+		listViewPlaylistItems = new ListView<Video>();		
+		listViewPlaylists = new ListView<Playlist>();
+		
+		listViewPlaylists.getSelectionModel().selectedItemProperty().addListener(new PlaylistChangeListener());
+		
+		ObservableList<Playlist> data = FXCollections.observableArrayList(playlists);
+		
+		listViewPlaylists.setItems(data);
+
 		
 		setCenter(listViewPlaylistItems);
 		setRight(listViewPlaylists);
 		
+	}
+	
+	private class PlaylistChangeListener implements ChangeListener<Playlist> {
+		
+		private PlaylistChangeListener() {
+			
+		}
+
+		@Override
+		public void changed(ObservableValue<? extends Playlist> ob, Playlist oldVal, Playlist newVal) {
+			ObservableList<Video> playlistItems = FXCollections.observableArrayList(newVal.getItems());
+			listViewPlaylistItems.setItems(playlistItems);
+		}
 	}
 }
