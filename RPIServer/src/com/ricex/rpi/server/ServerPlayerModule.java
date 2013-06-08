73a1444d1533fb49ebdaf8f227714415ea1412b8
RiceX1055 <computers1055@gmail.com>
2013-06-08 18:41:03 -0400

    Updated Playlist to include repeat and reapeatCurrent functionality. also changed ServerPlayerModule to only play Playlists, rather than video files

--- RPIServer/src/com/ricex/rpi/server/ServerPlayerModule.java
+++ RPIServer/src/com/ricex/rpi/server/ServerPlayerModule.java
@@ -1,14 +1,9 @@
 package com.ricex.rpi.server;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.ricex.rpi.common.PlayerModule;
-import com.ricex.rpi.common.RPIStatus;
 import com.ricex.rpi.common.message.IMessage;
 import com.ricex.rpi.common.message.MovieMessage;
-import com.ricex.rpi.server.client.Client;
 import com.ricex.rpi.server.client.RPIClient;
+import com.ricex.rpi.server.player.Playlist;
 
 /** Interface between the GUI and the Server
  * 
@@ -18,113 +13,98 @@ import com.ricex.rpi.server.client.RPIClient;
 
 public class ServerPlayerModule {
 
-	/** The list of clients to send the commands to */
-	private List<RPIClient> clients;
-	
-	/** Creates a new ServerPlayerModule with no clients */
-	
-	public ServerPlayerModule() {
-		this(new ArrayList<RPIClient>());
-	}
-	
-	/** Creates a new ServerPlayerModule with the given clients */
-	public ServerPlayerModule(List<RPIClient> clients) {
-		this.clients = clients;
-	}
-	                                            
+	/** The client to send the commands to */
+	private RPIClient client;
 
-	/** Starts playing the movie with the given path
+	/** The currently playing playlist */
+	private Playlist currentPlaylist;
+
+	/** Creates a new ServerPlayerModule with the given client */
+	public ServerPlayerModule(RPIClient client) {
+		this.client = client;
+	}
+
+
+	/** Starts playing the given play list
 	 * 
 	 * @param videoPath Path to the movie to play
 	 */
-	
-	public void play(String videoPath) {
-		sendMessage(new MovieMessage(videoPath, MovieMessage.Command.PLAY));		
+
+	public void play(Playlist playlist) {
+		currentPlaylist = playlist;
+		//sendMessage(new MovieMessage(videoPath, MovieMessage.Command.PLAY));
 	}
-	
+
 	/** Stops the currently playing movie */
-	
+
 	public void stop() {
 		sendMessage(new MovieMessage(MovieMessage.Command.STOP));
 	}
-	
+
 	/** Pauses / resumes the currently playing movie
 	 * 
 	 */
-	
+
 	public void pause() {
 		sendMessage(new MovieMessage(MovieMessage.Command.PAUSE));
 	}
-	
+
 	/** Move to the next chapter */
-	
+
 	public void nextChapter() {
 		sendMessage(new MovieMessage(MovieMessage.Command.NEXT_CHAPER));
 	}
-	
+
 	/** Move the previous chapter */
-	
+
 	public void previousChapter() {
 		sendMessage(new MovieMessage(MovieMessage.Command.PREVIOUS_CHAPTER));
 	}
-	
+
 	/** Turn the volume up */
-	
+
 	public void volumeUp() {
 		sendMessage(new MovieMessage(MovieMessage.Command.VOLUME_UP));
 	}
-	
+
 	/** Turn the volume down */
-	
+
 	public void volumeDown() {
 		sendMessage(new MovieMessage(MovieMessage.Command.VOLUME_DOWN));
 	}
-	
+
 	/** Seek to the right 30 seconds */
-	
+
 	public void seekForwardSlow() {
 		sendMessage(new MovieMessage(MovieMessage.Command.SEEK_FORWARD_SLOW));
 	}
-	
+
 	/** Seek to the right 600 seconds */
-	
+
 	public void seekForwardFast() {
 		sendMessage(new MovieMessage(MovieMessage.Command.SEEK_FORWARD_FAST));
 	}
-	
+
 	/** Seek to the left 30 seconds */
-	
+
 	public void seekBackwardSlow() {
 		sendMessage(new MovieMessage(MovieMessage.Command.SEEK_BACKWARD_SLOW));
 	}
-	
+
 	/** Seek to the right 600 seconds */
-	
+
 	public void seekBackwardFast() {
 		sendMessage(new MovieMessage(MovieMessage.Command.SEEK_BACKWARD_FAST));
 	}
-	
-	/** Sends a message to all of the clients conencted to the server,
-	 *  will obviouisly need to be refactored
-	 *  
+
+	/** Sends a message to this server play modules client
 	 * @param message The message to send
 	 */
-	
+
 	private void sendMessage(IMessage message) {
-		for (RPIClient client: clients) {
-			client.sendMessage(message);
-		}
+		client.sendMessage(message);
 	}
 
-	/** Adds the given client to the list to send commands to */
-	
-	public void addClient(RPIClient client) {
-		clients.add(client);
-	}	
-	
-	/** Removes the given client from the list to send commands to */
-	public void removeClient(RPIClient client) {
-		clients.remove(client);
-	}
+
 
 }
--- RPIServer/src/com/ricex/rpi/server/player/Playlist.java
+++ RPIServer/src/com/ricex/rpi/server/player/Playlist.java
@@ -15,61 +15,111 @@ public class Playlist {
 
 	/** The name of the play list */
 	private final String name;
-	
+
 	/** The items that the play list will play */
 	private List<Video> items;
-	
+
 	/** the index of the currently playing item */
 	private int currentIndex;
-	
+
+	/** Repeat the whole playlist once it is done playing */
+	private boolean repeat;
+
+	/** Repeat the current video when it is done playing */
+	private boolean repeatCurrent;
+
 	public Playlist(String name) {
 		this.name = name;
 		currentIndex = 0;
 		items = new ArrayList<Video>();
 	}
-	
+
 	/** Returns the name of this play list */
 	public String getName() {
 		return name;
 	}
-	
+
 	/** Adds the given item to the end of the play list
 	 * 
 	 * @param item The item to add
 	 */
-	
+
 	public void addItem(Video item) {
 		items.add(item);
 	}
-	
-	/** Removes the given item 
+
+	/** Removes the given item
 	 * 
 	 * @param item The item to remove
 	 */
-	
+
 	public void removeItem(Video item) {
 		items.remove(item);
 	}
-	
+
 	/** returns the list of items */
 	public List<Video> getItems() {
 		return items;
 	}
-	
-	/** Returns the next item to be played, or null if at the end 
-	 */	
+
+	/** Returns the next item to be played, or null if at the end
+	 */
 	public Video getNextItem() {
-		currentIndex++; //advance to the next item
-		if (items.size() < 1 || items.size() >= currentIndex) {
+		if (items.size() < 1) {
+			//the playlist is empty return null
 			return null;
+		}
+		if (repeatCurrent) {
+			return items.get(currentIndex);
+		}
+		currentIndex++; //advance to the next item
+		if (items.size() >= currentIndex) {
+			//currentIndex is over the limit.
+			if (repeat) {
+				// set the current index to the first item
+				currentIndex = 0;
+			}
+			else {
+				// not repeating and we made it to the end.
+				return null;
+			}
 		}
 		return items.get(currentIndex);
 	}
-	
+
+
+	/**
+	 * @return the repeat
+	 */
+	public boolean isRepeat() {
+		return repeat;
+	}
+
+	/**
+	 * @param repeat the repeat to set
+	 */
+	public void setRepeat(boolean repeat) {
+		this.repeat = repeat;
+	}
+
+	/**
+	 * @return the repeatCurrent
+	 */
+	public boolean isRepeatCurrent() {
+		return repeatCurrent;
+	}
+
+	/**
+	 * @param repeatCurrent the repeatCurrent to set
+	 */
+	public void setRepeatCurrent(boolean repeatCurrent) {
+		this.repeatCurrent = repeatCurrent;
+	}
+
 	/**
 	 * {@inheritDoc}
 	 */
-	
+
 	@Override
 	public String toString() {
 		return getName();
