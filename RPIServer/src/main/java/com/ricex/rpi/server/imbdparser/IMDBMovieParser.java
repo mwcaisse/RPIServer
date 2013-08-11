247343daf77575ec0f22693ffa1ed166b3b743e0
Mitchell <computers1055@gmail.com>
2013-08-11 00:18:22 -0400

    Added a basis for IMBD movie parser

--- RPIServer/src/main/java/com/ricex/rpi/server/imbdparser/IMDBMovieParser.java
+++ RPIServer/src/main/java/com/ricex/rpi/server/imbdparser/IMDBMovieParser.java
@@ -1,17 +1,23 @@
-package com.ricex.rpi.common.video;
+package com.ricex.rpi.server.imbdparser;
 
 import java.io.File;
 import java.io.FileFilter;
 
+import com.ricex.rpi.common.video.Directory;
+import com.ricex.rpi.common.video.Movie;
+import com.ricex.rpi.common.video.MovieParser;
+import com.ricex.rpi.common.video.Video;
 
 /**
- * Parses the movies from the given base folder
+ * Movie parser that reads data from XML meta file, as well as attempting to
+ * fetch data from IMBD
  * 
- * @author Mitchell Caisse
+ * 
+ * @author Mitchell
  * 
  */
 
-public class BasicMovieParser implements MovieParser {
+public class IMDBMovieParser implements MovieParser {
 
 	private static final String[] acceptedFileNames = { "avi", "mkv", "mp4" };
 
@@ -24,15 +30,16 @@ public class BasicMovieParser implements MovieParser {
 	/** Filter for filterting files based on directory, and extension */
 	private MovieFileFilter movieFilter;
 
-	public BasicMovieParser() {
+	public IMDBMovieParser() {
 		movieFilter = new MovieFileFilter();
 	}
-	
-	/** Sets the base folder to the given base folder
+
+	/**
+	 * Sets the base folder to the given base folder
 	 * 
 	 * @param baseFolder
 	 */
-	
+
 	private void setBaseFolder(String baseFolder) {
 		this.baseFolder = baseFolder;
 		baseFile = new File(baseFolder);
@@ -84,13 +91,16 @@ public class BasicMovieParser implements MovieParser {
 	private String getRelativeFilePath(File file) {
 		return baseFile.toURI().relativize(file.toURI()).getPath();
 	}
-	
-	/** Parses the name of the movie from the file name
+
+	/**
+	 * Parses the name of the movie from the file name
 	 * 
-	 * @param fileName The name of the file
-	 * @return The name of the movie, or the name of the file if movie name could not be parsed
+	 * @param fileName
+	 *            The name of the file
+	 * @return The name of the movie, or the name of the file if movie name
+	 *         could not be parsed
 	 */
-	
+
 	private String parseMovieName(String fileName) {
 		int lastDot = fileName.lastIndexOf(".");
 		String name = fileName.substring(0, lastDot);
@@ -115,4 +125,5 @@ public class BasicMovieParser implements MovieParser {
 		}
 
 	}
+
 }
